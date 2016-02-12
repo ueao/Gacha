915b0c3a48fd21cc39df72912644c6cd58820a75
Aoi Sakaue <aoiue.w.bbb@gmail.com>
2016-02-12 10:06:17 +0900

    ガチャガチャゲームアプリです。一部未だエラーあり。

---
+++ Gachapin/ResultViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.swift
+//  Gachapin
+//
+//  Created by Aoi Sakaue on 2016/02/11.
+//  Copyright © 2016年 Aoi Sakaue. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: NSObject {
+
+}
---
+++ ResultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultViewController.swift
+//  Gachapin
+//
+//  Created by Aoi Sakaue on 2016/02/11.
+//  Copyright © 2016年 Aoi Sakaue. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
