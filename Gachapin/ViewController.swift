ae73bd7e215626242edec27c35e51a8574810350
Aoi Sakaue <aoiue.w.bbb@gmail.com>
2016-03-03 13:35:54 +0900

    no message

--- Gachapin/ViewController.swift
+++ Gachapin/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func gacha(){
+//SegueのIdentifierが"result"のViewへ画面遷移
+        self.performSegueWithIdentifier("result",sender:nil)
+    }
 
 }
 
--- ResultViewController.swift
+++ ResultViewController.swift
@@ -9,18 +9,85 @@
 import UIKit
 
 class ResultViewController: UIViewController {
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+//使う部品を宣言する
+    @IBOutlet var haikeiImageView: UIImageView!
+    
+    @IBOutlet var monsterImageView: UIImageView!
+    
+    var monsterArray:[UIImage]!
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let animation = CABasicAnimation(keyPath: "transform")
+        animation.fromValue = NSNumber(double: 0)
+        
+        animation.toValue = NSNumber(double: 2 * M_PI)
+        
+        animation.valueFunction = CAValueFunction(name:kCAValueFunctionRotateZ)
+        
+        animation.duration = 5
+        
+        animation.repeatCount = Float.infinity
+        
+        haikeiImageView.layer.addAnimation(animation, forKey: nil)
     }
-
+    
+    
+    @IBAction func modoru() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+
+
+//ランダムにモンスターを表示させる
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        //このResulltViewControllerが表示されるたびに呼び出すメソッド
+        
+        // Do any additional setup after loading the view.
+
+        let number = Int(rand() % 9)
+        
+//monsterArray配列に画像を10枚追加（保存）する
+        monsterArray = [UIImage(named: "monster001.png")!,
+            UIImage(named: "monster002.png")!,
+            UIImage(named: "monster003.png")!,
+            UIImage(named: "monster004.png")!,
+            UIImage(named: "monster005.png")!,
+            UIImage(named: "monster006.png")!,
+            UIImage(named: "monster007.png")!,
+            UIImage(named: "monster008.png")!,
+            UIImage(named: "monster009.png")!,
+            UIImage(named: "monster010.png")!
+        ]
+//monsterArrayの中のnumber番目を表示させる（ランダムに表示させる）
+        monsterImageView.image = monsterArray[number]
+        
+//numberの数字により背景画像を切り替える
+        if number == 9 {
+            haikeiImageView.image = UIImage(named: "bg_gold.ong")
+        }else if number > 6{
+            haikeiImageView.image = UIImage(named: "bg_red.png")
+        }else {
+            haikeiImageView.image = UIImage(named: "bg_blue.png")
+        }
+    
+
+    }
+    
+    }
 
     /*
     // MARK: - Navigation
@@ -30,6 +97,7 @@ class ResultViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+ */
 
-}
+
+
